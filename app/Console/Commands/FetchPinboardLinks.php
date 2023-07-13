54147c71c56d6bdea5501af66161ccabff6e487b
Macit Simsek <macitsimsek12@hotmail.com>
2023-07-13 19:21:33 +0100

    Check internet connection to avoid misleading validity of the urls, move the command functioanlity to service for reusability

--- app/Console/Commands/FetchPinboardLinks.php
+++ app/Console/Commands/FetchPinboardLinks.php
@@ -2,11 +2,8 @@
 
 namespace App\Console\Commands;
 
-use App\Models\Link;
 use App\Services\LinkService;
-use GuzzleHttp\Client;
 use Illuminate\Console\Command;
-use Symfony\Component\DomCrawler\Crawler;
 
 class FetchPinboardLinks extends Command
 {
@@ -14,9 +11,8 @@ class FetchPinboardLinks extends Command
 
     protected $description = 'Command description';
 	
-	// Use dependency injection to inject the HTTP client as a service, inject link service
+	// Use dependency injection to inject link service
 	public function __construct(
-		protected Client $client,
 		protected LinkService $linkService
 	)
 	{
@@ -25,44 +21,21 @@ class FetchPinboardLinks extends Command
 	
 	public function handle()
 	{
-		// Use the injected client to make the HTTP request
-		$response = $this->client->get('https://pinboard.in/u:alasdairw?per_page=120');
-		$html = $response->getBody()->getContents();
+		// Check internet connection to avoid misleading validity of the urls
+		if (!$this->linkService->checkInternetConnection()) {
+			$this->info('Please connect to internet');
+			return;
+		}
 		
-		$crawler = new Crawler($html);
-		$crawler->filter('div.bookmark')->each(function (Crawler $tr){
-			// Use the Crawler to filter and query the HTML with link has tag class
-			$tags = $tr->filter('a.tag');
-			//Check if the link has tags
-			if ($tags->count() > 0) {
-				$tagsArray = $tags->each(function ($tag) {
-					return $tag->text();
-				});
-				// Check links tags are matching one of the wanted tags
-				$match = $this->linkService->checkIfTagExists($tagsArray);
-				if ($match) {
-					// Filter data from html
-					$linkElement = $tr->filter('a.bookmark_title');
-					$comments = $tr->filter('div.description')->text();
-					$title = $linkElement->text();
-					$href = $linkElement->attr('href');
-					
-					$newLink = new Link();
-					$validity = $this->linkService->isUrlValid($href);
-					$newLink->fill([
-						'title' => $title,
-						'href' => $href,
-						'comments' => $comments,
-						'valid' => $validity
-					]);
-					$newLink->save();
-					
-					// Save the tags to table and attach to link
-					$ids = $this->linkService->addTagsAndGetIds($tagsArray);
-					$newLink->tags()->attach($ids);
-				}
-			}
-		});
+		$this->info('Link processing started');
+		
+		$count = $this->linkService->startProcessing();
+		
+		if ($count > 0) {
+			$this->info($count . ' Link added');
+		} else {
+			$this->info('No new Link found');
+		}
 		
 		$this->info('Link processing finished');
 	}
--- app/Services/LinkService.php
+++ app/Services/LinkService.php
@@ -2,13 +2,22 @@
 
 namespace App\Services;
 
+use App\Models\Link;
 use App\Models\Tag;
 use GuzzleHttp\Client;
 use GuzzleHttp\Exception\GuzzleException;
 use GuzzleHttp\RequestOptions;
+use Symfony\Component\DomCrawler\Crawler;
 
 class LinkService
 {
+	// Use dependency injection to inject the HTTP client as a service
+	public function __construct(
+		protected Client $client
+	)
+	{
+	}
+	
 	public function isUrlValid($url):bool
 	{
 		$client = new Client([
@@ -56,6 +65,75 @@ class LinkService
 		} else {
 			// No matching elements found
 			return false;
+		}
+	}
+	
+	public function startProcessing():int {
+		$totalLink = 0;
+		// Use the injected client to make the HTTP request
+		$response = $this->client->get('https://pinboard.in/u:alasdairw?per_page=120');
+		$html = $response->getBody()->getContents();
+		
+		$crawler = new Crawler($html);
+		$crawler->filter('div.bookmark')->each(function (Crawler $tr) use (&$totalLink) {
+			// Use the Crawler to filter and query the HTML with link has tag class
+			$tags = $tr->filter('a.tag');
+			//Check if the link has tags
+			if ($tags->count() > 0) {
+				$tagsArray = $tags->each(function ($tag) {
+					return $tag->text();
+				});
+				// Check links tags are matching one of the wanted tags
+				$match = $this->checkIfTagExists($tagsArray);
+				if ($match) {
+					// Filter data from html
+					$linkElement = $tr->filter('a.bookmark_title');
+					$comments = $tr->filter('div.description')->text();
+					$title = $linkElement->text();
+					$href = $linkElement->attr('href');
+					$result = $this->createLinksAndAttachToTags($title,$href,$comments,$tagsArray);
+					
+					if ($result) {
+						$totalLink++;
+					}
+				}
+			}
+		});
+		
+		return $totalLink;
+	}
+	
+	public function createLinksAndAttachToTags($title,$href,$comments,$tagsArray): bool{
+		$newLink = new Link();
+		$checkLinkExists = Link::where('href', $href)
+			->where('title',$title)
+			->exists();
+		if (!$checkLinkExists) {
+			$validity = $this->isUrlValid($href);
+			$newLink->fill([
+				'title' => $title,
+				'href' => $href,
+				'comments' => $comments,
+				'valid' => $validity
+			]);
+			$newLink->save();
+			// Save the tags to table and attach to link
+			$ids = $this->addTagsAndGetIds($tagsArray);
+			$newLink->tags()->attach($ids);
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
+	public function checkInternetConnection($host = 'www.example.com', $port = 80, $timeout = 5):bool {
+		$fp = @fsockopen($host, $port, $errno, $errstr, $timeout);
+		
+		if ($fp) {
+			fclose($fp);
+			return true; // Internet connection is available
+		} else {
+			return false; // No internet connection
 		}
 	}
 }
